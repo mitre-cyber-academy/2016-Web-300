a03f040ebab1bdb413114d883f767a07980511bc
Kyle Fagan <bialogs@gmail.com>
2016-06-30 14:58:26 -0400

    Updated flag and password

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-first_user = User.create(name: 'jon', email: 'j0n@j0n.com', password: '5a27dfc49029a8740cf1d1a2af118a94', password_confirmation: '5a27dfc49029a8740cf1d1a2af118a94')
-Message.create(title: 'Hey there', body: 'Need to remember this: MCA-A342B91C', receiving_user: first_user, sending_user: first_user)
+first_user = User.create(name: 'fank', email: 'fank@fank.com', password: 'f2rekus5eX_P8ya5ru2rapREyeruf83w', password_confirmation: 'f2rekus5eX_P8ya5ru2rapREyeruf83w')
+Message.create(title: 'Hey there', body: 'Need to remember this: 763d661f088cbd9e83ed2d3f6c4865cca6f1d284', receiving_user: first_user, sending_user: first_user)
